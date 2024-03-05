7c5553383ece3ecc0eac9a83adc69946cb43c0be
marthydavid <marthy.david@gmail.com>
2024-03-05 08:37:16 +0100

    Switch url for sshpass tgz

--- sshpass.rb
+++ sshpass.rb
@@ -1,6 +1,6 @@
 class Sshpass < Formula
   homepage "https://sourceforge.net/projects/sshpass/"
-  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.10/sshpass-1.10.tar.gz"
+  url "https://github.com/marthydavid/homebrew-sshpass/releases/download/1.10/sshpass-1.10.tar.gz"
   sha256 "ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda"
 
   def install
